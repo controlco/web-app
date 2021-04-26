381298872d1670496b6b597fa8a6e9e1f2abebc0
nabenitez <nabenitez@currencybird.cl>
2021-04-25 20:28:38 -0400

    add: PropertyItem component

---
+++ src/components/Properties/PropertyItem/PropertyItem.styles.js
@@ -0,0 +1,8 @@
+import { createStyles, makeStyles } from "@material-ui/core/styles";
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    root: {},
+    container: {},
+  })
+);
+export default useStyles;
