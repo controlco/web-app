b9de45b484ad9c636f07100e1ad5452059dd604f
nabenitez <nabenitez@currencybird.cl>
2021-04-25 20:26:44 -0400

    add: navbar show

--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,11 @@ import React from "react";
 import Head from "next/head";
 import { ThemeProvider } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
+import NavBar from "../src/components/NavBar";
 import theme from "../src/theme";
 
 const MyApp = ({ Component, pageProps }) => {
+  const user = true;
   React.useEffect(() => {
     // Remove the server-side injected CSS.
     const jssStyles = document.querySelector("#jss-server-side");
@@ -25,6 +27,7 @@ const MyApp = ({ Component, pageProps }) => {
       <ThemeProvider theme={theme}>
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
+        {user && <NavBar />}
         <Component {...pageProps} />
       </ThemeProvider>
     </React.Fragment>
