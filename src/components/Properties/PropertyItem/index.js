942cf4f903b89f975916375929709187944d04e2
nabenitez <nabenitez@currencybird.cl>
2021-04-25 20:28:38 -0400

    add: PropertyItem component

---
+++ src/components/Properties/PropertyItem/index.js
@@ -0,0 +1,122 @@
+import React from "react";
+import { Container, TextField, Grid, Button } from "@material-ui/core";
+import Image from "next/image";
+
+import { useFormik } from "formik";
+
+const PropertyItem = (props) => {
+  const {
+    title,
+    description,
+    address,
+    phoneNumber,
+    imageUrl,
+    dimension,
+  } = props;
+  const formik = useFormik({
+    initialValues: {
+      title: title,
+      description: description,
+      address: address,
+      phoneNumber: phoneNumber,
+      imageUrl: imageUrl,
+      dimension: dimension,
+    },
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+  return (
+    <Container maxWidth="md">
+      <form onSubmit={formik.handleSubmit}>
+        <Grid container>
+          <Grid item container xs={12} md={6}>
+            <Image src={imageUrl} alt="me" width="auto" height="400px" />
+          </Grid>
+          <Grid item container xs={12} md={6}>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                label="Título"
+                name="title"
+                value={formik.values.title}
+                onChange={formik.handleChange}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                label="Descripción"
+                name="description"
+                value={formik.values.description}
+                onChange={formik.handleChange}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                label="Dirección"
+                name="address"
+                value={formik.values.address}
+                onChange={formik.handleChange}
+              />
+            </Grid>
+
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                label="Número de teléfono"
+                name="phoneNumber"
+                value={formik.values.phoneNumber}
+                onChange={formik.handleChange}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                label="Dimensión"
+                name="dimension"
+                value={formik.values.dimension}
+                onChange={formik.handleChange}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              {/* TODO: Implementar el subir imagenes y enviar a la API*/}
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                label="URL de magen"
+                name="imageUrl"
+                value={formik.values.imageUrl}
+                onChange={formik.handleChange}
+              />
+            </Grid>
+
+            <Button color="primary" variant="contained" fullWidth type="submit">
+              Guardar
+            </Button>
+          </Grid>
+        </Grid>
+      </form>
+    </Container>
+  );
+};
+
+export default PropertyItem;
