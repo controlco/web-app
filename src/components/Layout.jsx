50a4debc2ff674309094aad3c15b8d72ea0e6eb2
nabenitez <nabenitez@uc.cl>
2021-05-31 15:33:57 -0400

    remove: layout logs

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -7,9 +7,9 @@ import Loading from './Loading';
 const Layout = ({ children }) => {
   const { isAuthenticated, loading, user } = useAuth();
 
-  console.log(`auth in layout ${isAuthenticated}`);
+  /* console.log(`auth in layout ${isAuthenticated}`);
   console.log(`isLoading in layout${loading}`);
-  console.log(`user in layout${JSON.stringify(user)}`);
+  console.log(`user in layout${JSON.stringify(user)}`); */
   if (isAuthenticated) {
     return (
       <>
